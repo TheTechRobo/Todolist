6335639e62fc0bd33df8f4ef6b76d4d5f6092461
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-05-17 09:27:15 -0400

    less "hacking"

--- todo.py
+++ todo.py
@@ -1,12 +1,10 @@
 # IMPORT MODULES
 from sys import exit as e
+import six
 # DECLARE CONSTANTS
 VERSION = "N/A" #version of Todolist
 # detect python version
-try: 
-    hi = raw_input #will raise a NameError if Python 3.x
-    PYTHON_TWO = True #because if that completed successfully, it is python 2.x
-except NameError:
-    PYTHON_TWO = False
-if PYTHON_TWO is True:
-    e("Unsupported Python version: 2.x")
+if six.PY2: #source https://stackoverflow.com/a/43481041/9654083
+    e("Unsupported Python Version")
+else:
+    print("Welcome to Todolist!")
